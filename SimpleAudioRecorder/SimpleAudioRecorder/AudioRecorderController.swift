b3a02885e04b152a0aa292f289aa7bd268a08e5e
Paul Solt <PaulSolt@gmail.com>
2019-10-01 16:16:50 -0400

    Added starter project

---
+++ SimpleAudioRecorder/SimpleAudioRecorder/AudioRecorderController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  AudioRecorder
+//
+//  Created by Paul Solt on 10/1/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class AudioRecorderController: UIViewController {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeRemainingLabel: UILabel!
+    @IBOutlet weak var timeSlider: UISlider!
+	
+	private lazy var timeFormatter: DateComponentsFormatter = {
+		let formatting = DateComponentsFormatter()
+		formatting.unitsStyle = .positional // 00:00  mm:ss
+		// NOTE: DateComponentFormatter is good for minutes/hours/seconds
+		// DateComponentsFormatter not good for milliseconds, use DateFormatter instead
+		formatting.zeroFormattingBehavior = .pad
+		formatting.allowedUnits = [.minute, .second]
+		return formatting
+	}()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+
+        timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeLabel.font.pointSize,
+                                                          weight: .regular)
+        timeRemainingLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeRemainingLabel.font.pointSize,
+                                                                   weight: .regular)
+	}
+
+
+    @IBAction func playButtonPressed(_ sender: Any) {
+
+	}
+    
+    @IBAction func recordButtonPressed(_ sender: Any) {
+    
+    }
+}
+
